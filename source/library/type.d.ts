b9ed01a58785469fcdafe7428cca8583e559b721
H2Owater425 <h2o@h2owr.xyz>
2022-07-13 14:49:48 +0900

    feat: Add types for convenience

--- source/library/type.d.ts
+++ source/library/type.d.ts
@@ -5,6 +5,7 @@ import { HttpError, HttpErrorInformation } from '@library/httpError';
 import { RouteGenericInterface } from 'fastify/types/route';
 import { ResolveFastifyRequestType } from 'fastify/types/type-provider';
 import { REQUIRED_ENVIRONMENT_VARIABLE_NAMES } from '@library/environment';
+import { ArraySchema, BooleanSchema, IntegerSchema, NumberSchema, ObjectSchema, S, StringSchema } from 'fluent-json-schema';
 
 declare global {
 	namespace NodeJS {
@@ -33,7 +34,6 @@ interface Reply extends RouteGenericInterface {
 declare module 'fastify' {
   interface FastifyRequest {
     user: {
-			rateLimit: number;
 			id: string;
 			permission: number;
 		}
@@ -42,18 +42,26 @@ declare module 'fastify' {
 	type PayloadReply = FastifyReply<Server, IncomingMessage, ServerResponse, Reply, unknown, FastifySchema, FastifyTypeProvider, Reply['Reply']>;
 }
 
-interface Page {
-	size: number;
-	index: number;
-}
-
 interface RouteOptions extends Omit<_RouteOptions, 'handler'> {
 	method: HTTPMethods;
-	handler: RouteHandlerMethod<Server, IncomingMessage, ServerResponse, any, unknown, FastifySchema, FastifyTypeProvider, ResolveFastifyRequestType<FastifyTypeProvider, FastifySchema, any>, FastifyLoggerInstance>;
+	//handler: RouteHandlerMethod<Server, IncomingMessage, ServerResponse, any, unknown, FastifySchema, FastifyTypeProvider, ResolveFastifyRequestType<FastifyTypeProvider, FastifySchema, any>, FastifyLoggerInstance>;
+	handler: RouteHandlerMethod<Server, IncomingMessage, ServerResponse, any, any, FastifySchema, FastifyTypeProvider, any, FastifyLoggerInstance>;
 }
 
 interface ModuleOptions {
 	routers: RouteOptions[];
 	modules?: Module[];
 	prefix?: string;
+}
+
+type RecursiveRecord<T extends string | number | symbol, S> = {
+	[key in T]: S | RecursiveRecord<T, S>;
+};
+
+export type Schema<T extends string> = Record<T, ObjectSchema | StringSchema | NumberSchema | ArraySchema | IntegerSchema | BooleanSchema>;
+
+export interface PageQuery {
+	'page[size]'?: number;
+	'page[index]'?: number;
+	'page[order]'?: 'desc' | 'asc';
 }
