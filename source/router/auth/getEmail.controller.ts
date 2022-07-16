47c86b192d138eb1923d53f18035d4f3cf18722d
H2Owater425 <h2o@h2owr.xyz>
2022-07-16 16:00:11 +0900

    feat: Add document on email verification

--- source/router/auth/getEmail.controller.ts
+++ source/router/auth/getEmail.controller.ts
@@ -4,12 +4,27 @@ import { PayloadReply, FastifyRequest } from 'fastify';
 
 export default function (request: FastifyRequest<{ Querystring: Pick<User, 'verificationKey'> }>, reply: PayloadReply): void {
 	prisma.user.findFirst({
-		select: { id: true },
+		select: {
+			id: true,
+			name: true
+		},
 		where: { verificationKey: request['query']['verificationKey'] }
 	})
-	.then(function (user: Pick<User, 'id'> | null): void {
+	.then(function (user: Pick<User, 'id' | 'name'> | null): void {
 		if(user !== null) {
-			reply.redirect('https://dimi.wiki/login?emailAuth=true'); // TODO: change when frontend development is complete
+			prisma.document.create({
+				select: null,
+				data: {
+					title: user['name'],
+					type: DocumentTypeIndex['user'],
+					plain: '',
+					content: ''
+				}
+			})
+			.then(function (): void {
+				reply.redirect('https://dimi.wiki/login?emailAuth=true'); // TODO: change when frontend development is complete
+			})
+			.catch(reply.send.bind(reply));
 		} else {
 			reply.callNotFound();
 		}
